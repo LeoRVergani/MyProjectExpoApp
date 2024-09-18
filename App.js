1b2583db2de059f4b0273ae576908afcf53db09f
leorvergani <vergani@outlook.com.br>
2024-09-18 19:45:31 -0300

    treinamento

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+async function setItem() {
+  try {
+    await AsyncStorage.setItem('theme'. isDark);
+    console.log('Dado foi salvo')
+  } catch (error) {
+    console.log()
+  }
+
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Treinamento de asyncStorage</Text>
       <StatusBar style="auto" />
     </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
